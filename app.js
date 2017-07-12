52cb81c6d0f75b9808be0235f2f27999ed01849d
Sally Bouley <sallybouley87@gmail.com>
2017-07-12 08:52:34 -0600

    minor code cleanup

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ var calculate = require('./calc')
 var sum = require('./sum')
 var bodyParser = require('body-parser')
 
-
-// app.use(express.json());       // to support JSON-encoded bodies
-// app.use(express.urlencoded());
-
 app.get('/math/pi', pi);
 app.get('/math/calculate', calculate);
 app.post('/math/sum', sum);
